ad2669887da548f85fcfa089b112787cb4ed6fa8
Unknown <danieljerezgarrido@gmail.com>
2017-12-24 20:03:32 +0100

    load settings, languages and combo box auto load

--- launcher imperivm iii/launcher imperivm iii/Form1.Designer.cs
+++ launcher imperivm iii/launcher imperivm iii/Form1.Designer.cs
@@ -33,12 +33,13 @@
             this.play = new System.Windows.Forms.Button();
             this.labelLanguage = new System.Windows.Forms.Label();
             this.language = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // resolution
             // 
             this.resolution.FormattingEnabled = true;
-            this.resolution.Location = new System.Drawing.Point(139, 324);
+            this.resolution.Location = new System.Drawing.Point(74, 39);
             this.resolution.Name = "resolution";
             this.resolution.Size = new System.Drawing.Size(121, 21);
             this.resolution.TabIndex = 0;
@@ -46,7 +47,7 @@
             // labelResolution
             // 
             this.labelResolution.AutoSize = true;
-            this.labelResolution.Location = new System.Drawing.Point(76, 327);
+            this.labelResolution.Location = new System.Drawing.Point(11, 42);
             this.labelResolution.Name = "labelResolution";
             this.labelResolution.Size = new System.Drawing.Size(57, 13);
             this.labelResolution.TabIndex = 1;
@@ -54,37 +55,48 @@
             // 
             // play
             // 
-            this.play.Location = new System.Drawing.Point(277, 322);
+            this.play.Location = new System.Drawing.Point(212, 37);
             this.play.Name = "play";
             this.play.Size = new System.Drawing.Size(75, 23);
             this.play.TabIndex = 2;
-            this.play.Text = "Jugar";
+            this.play.Text = "Play";
             this.play.UseVisualStyleBackColor = true;
             this.play.Click += new System.EventHandler(this.button1_Click);
             // 
             // labelLanguage
             // 
             this.labelLanguage.AutoSize = true;
-            this.labelLanguage.Location = new System.Drawing.Point(76, 300);
+            this.labelLanguage.Location = new System.Drawing.Point(11, 15);
             this.labelLanguage.Name = "labelLanguage";
             this.labelLanguage.Size = new System.Drawing.Size(55, 13);
             this.labelLanguage.TabIndex = 4;
-            this.labelLanguage.Text = "Lenguage";
+            this.labelLanguage.Text = "Language";
             // 
             // language
             // 
             this.language.FormattingEnabled = true;
-            this.language.Location = new System.Drawing.Point(139, 297);
+            this.language.Location = new System.Drawing.Point(74, 12);
             this.language.Name = "language";
             this.language.Size = new System.Drawing.Size(121, 21);
             this.language.TabIndex = 3;
-            this.language.SelectedIndexChanged += new System.EventHandler(this.language_SelectedIndexChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(71, 73);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(156, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "daniel.jerez@fxgamestudio.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 374);
+            this.ClientSize = new System.Drawing.Size(304, 100);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.labelLanguage);
             this.Controls.Add(this.language);
             this.Controls.Add(this.play);
@@ -94,6 +106,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Imperivm III HD - Beta";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +119,7 @@
         private System.Windows.Forms.Button play;
         private System.Windows.Forms.Label labelLanguage;
         private System.Windows.Forms.ComboBox language;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- launcher imperivm iii/launcher imperivm iii/Form1.cs
+++ launcher imperivm iii/launcher imperivm iii/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,23 +17,68 @@ namespace launcher_imperivm_iii
         public Form1()
         {
             InitializeComponent();
-            IniParser parser = new IniParser(@"Settings.ini");
-            parser.AddSetting("Language", "Default", "English");
-            parser.SaveSettings();
-            String  languaje = parser.GetSetting("Language", "Default");
-            
-            labelLanguage.Text = languaje;
-            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
+
+            IniParser parser = new IniParser(@"Settings.ini");
+
+            parser.AddSetting("Language", "Default", language.Text);
+            parser.AddSetting("Options", "Resolution", resolution.SelectedIndex.ToString());
+
+            parser.SaveSettings();
+
             Process.Start(@"gbr.exe");
+
+            Application.Exit();
+
+
         }
 
-        private void language_SelectedIndexChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            
+
+            var pakLanguages = new DirectoryInfo("local").GetFiles("*.pak");
+            for(int i = 0; i < pakLanguages.Length; i++)
+            {
+                language.Items.Add(FirstCharToUpper(pakLanguages[i].Name.Split('.')[0]));
+            }
+
+            IniParser parser = new IniParser(@"Settings.ini");
+            String languageDefault = (parser.GetSetting("Language", "Default")).ToUpper();
+
+            language.SelectedIndex = language.FindStringExact(languageDefault);
+
+            String resolutionDefault = (parser.GetSetting("Options", "Resolution"));
+
+            resolution.Items.Add("1024 x 768");
+            resolution.Items.Add("1152 x 864");
+            resolution.Items.Add("1280 x 1024");
+            resolution.Items.Add("1280 x 720");
+            resolution.Items.Add("1366 x 768");
+            resolution.Items.Add("1440 x 900");
+            resolution.Items.Add("1920 x 1080");
+
+            resolution.SelectedIndex = int.Parse(resolutionDefault);
+
+        }
+
+
+        public static string FirstCharToUpper(string input)
+        {
+            switch (input)
+            {
+                case null: throw new ArgumentNullException(nameof(input));
+                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+                default: return input.First().ToString().ToUpper() + input.Substring(1);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:daniel.jerez@fxgamestudio.com");
         }
     }
 }
