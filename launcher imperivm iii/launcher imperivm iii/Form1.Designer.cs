b87854e147b19099f229ac5e8fe7b66bc996b1d2
Unknown <danieljerezgarrido@gmail.com>
2017-12-24 18:53:27 +0100

    first load settings.ini

---
+++ launcher imperivm iii/launcher imperivm iii/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace launcher_imperivm_iii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resolution = new System.Windows.Forms.ComboBox();
+            this.labelResolution = new System.Windows.Forms.Label();
+            this.play = new System.Windows.Forms.Button();
+            this.labelLanguage = new System.Windows.Forms.Label();
+            this.language = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // resolution
+            // 
+            this.resolution.FormattingEnabled = true;
+            this.resolution.Location = new System.Drawing.Point(139, 324);
+            this.resolution.Name = "resolution";
+            this.resolution.Size = new System.Drawing.Size(121, 21);
+            this.resolution.TabIndex = 0;
+            // 
+            // labelResolution
+            // 
+            this.labelResolution.AutoSize = true;
+            this.labelResolution.Location = new System.Drawing.Point(76, 327);
+            this.labelResolution.Name = "labelResolution";
+            this.labelResolution.Size = new System.Drawing.Size(57, 13);
+            this.labelResolution.TabIndex = 1;
+            this.labelResolution.Text = "Resolution";
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(277, 322);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(75, 23);
+            this.play.TabIndex = 2;
+            this.play.Text = "Jugar";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelLanguage
+            // 
+            this.labelLanguage.AutoSize = true;
+            this.labelLanguage.Location = new System.Drawing.Point(76, 300);
+            this.labelLanguage.Name = "labelLanguage";
+            this.labelLanguage.Size = new System.Drawing.Size(55, 13);
+            this.labelLanguage.TabIndex = 4;
+            this.labelLanguage.Text = "Lenguage";
+            // 
+            // language
+            // 
+            this.language.FormattingEnabled = true;
+            this.language.Location = new System.Drawing.Point(139, 297);
+            this.language.Name = "language";
+            this.language.Size = new System.Drawing.Size(121, 21);
+            this.language.TabIndex = 3;
+            this.language.SelectedIndexChanged += new System.EventHandler(this.language_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 374);
+            this.Controls.Add(this.labelLanguage);
+            this.Controls.Add(this.language);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.labelResolution);
+            this.Controls.Add(this.resolution);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Imperivm III HD - Beta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox resolution;
+        private System.Windows.Forms.Label labelResolution;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Label labelLanguage;
+        private System.Windows.Forms.ComboBox language;
+    }
+}
+
---
+++ launcher imperivm iii/launcher imperivm iii/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace launcher_imperivm_iii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            IniParser parser = new IniParser(@"Settings.ini");
+            parser.AddSetting("Language", "Default", "English");
+            parser.SaveSettings();
+            String  languaje = parser.GetSetting("Language", "Default");
+            
+            labelLanguage.Text = languaje;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"gbr.exe");
+        }
+
+        private void language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
